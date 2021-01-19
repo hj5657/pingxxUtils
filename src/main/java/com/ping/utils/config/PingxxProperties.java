356cdcf03b211231face8c4354f5507e399b814d
Changle Zhang <changle.zhang@ingka.com>
2021-01-19 09:44:11 +0800

    add pingxx properties

---
+++ src/main/java/com/ping/utils/config/PingxxProperties.java
@@ -0,0 +1,17 @@
+package com.ping.utils.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class PingxxProperties {
+
+  @Value("${pingApp.apiKey}")
+  private String apiKey;
+  @Value("${pingApp.appId}")
+  private String appId;
+  @Value("${pingApp.limit:100}")
+  private Integer limit;
+}
--- src/main/java/com/ping/utils/service/PingService.java
+++ src/main/java/com/ping/utils/service/PingService.java
@@ -1,30 +1,33 @@
 package com.ping.utils.service;
 
+import static com.pingplusplus.Pingpp.apiKey;
+
+import com.ping.utils.config.PingxxProperties;
 import com.pingplusplus.Pingpp;
 import com.pingplusplus.exception.PingppException;
 import com.pingplusplus.model.Charge;
 import com.pingplusplus.model.ChargeCollection;
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PingService {
 
   private final static String privateKeyFilePath = "src/main/resources/ping-key.pem";
-  @Value("${pingApp.apiKey}")
-  private String apiKey;
-  @Value("${pingApp.appId}")
-  private String appId;
+  private final PingxxProperties pingxxProperties;
+
+  public PingService(PingxxProperties pingxxProperties) {
+    this.pingxxProperties = pingxxProperties;
+  }
 
   public String getPingSerialId(String requestOrderId)
       throws PingppException {
     Pingpp.privateKeyPath = privateKeyFilePath;
-    Pingpp.apiKey = apiKey;
+    apiKey = pingxxProperties.getApiKey();
     Map<String, Object> chargeParams = new HashMap<>();
-    chargeParams.put("limit", 100);
-    chargeParams.put("app[id]", appId);
+    chargeParams.put("limit", pingxxProperties.getLimit());
+    chargeParams.put("app[id]", pingxxProperties.getAppId());
     ChargeCollection chargeList = Charge.list(chargeParams);
     for (Charge item : chargeList.getData()) {
       if (item.getOrderNo().startsWith(requestOrderId)) {
